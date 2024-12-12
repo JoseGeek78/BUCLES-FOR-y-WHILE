beb49ca6e40ce8bf01a1da5d68524f3fcee04e6e
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-12-12 14:56:56 +0100

    .

--- bucles.py
+++ bucles.py
@@ -1,9 +1,12 @@
-colores = ['Rojo', 'Azul', 'Blanco', 'Amarillo', 'Verde']
-print('---LISTADO DE COLORES---')
 
-for color in colores:
-    print(f'-{color}.')
 
+
+
+# colores = ['Rojo', 'Azul', 'Blanco', 'Amarillo', 'Verde']
+# print('---LISTADO DE COLORES---')
+
+# for color in colores:
+#    print(f'-{color}.')
 
 # for i in range(5,-24,-3):
 #    print(f'El valor del bucle es: {i}.')
